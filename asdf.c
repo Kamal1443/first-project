0c88aeb283cb989a198147c7bb61937571e12e17
unknown <=>
2021-11-02 20:05:48 +0500

    printf o'zgartirildi

--- asdf.c
+++ asdf.c
@@ -1,21 +1,10 @@
+#include <stdio.h>
 #include <stdlib.h>
-#include <stdlib.h>
 
-int main() 
-{  
-    char color[20];
-    char pluralNoun[20];
-    char celebrity[20];
-
-    printf ("Enter a color: ");
-    scanf("%s", color);
-    printf ("Enter a plural noun: ");
-    scanf("%s", pluralNoun);
-    printf ("Enter a celebrity: ");
-    scanf("%s", celebrity);
-    printf ("Roses are %s\n");
-    printf ("%s are blue\n");
-    printf ("I love %s\n");
-
+int main () {
+    double num1;
+    double num2;
+    char op;
+    
     return 0;
-} 
+}
